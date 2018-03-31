25818aa6e47bc71b1f251bc601e8ea793a75d24a
AndresMorenoR <alkokmoren27@gmail.com>
2018-03-31 12:45:11 -0500

    #1
    Se realiza el registro de usuario nuevo

--- petRecovery/index.php
+++ petRecovery/index.php
@@ -15,7 +15,7 @@
  	<p><input type="text" id="caja1" name ="usuario" placeholder="Usuario"></p>
 </label> 
 <label for="caja2"><p>Contraseña</p>
- 	<p><input type="password" id="caja2" name="password" placeholder="Contraseña"></p>
+ 	<p><input type="password" id="caja2" name="password" placeholder="Contraseña" required></p>
 </label>
 <label for="caja3"><p>correo</p>
  	<p><input type="text" id="caja3" name ="correo" placeholder="Correo electronio"></p>
@@ -33,28 +33,3 @@
 	
 </body>
 </html>
-
-
-
-<!--
-<!doctype <html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title> Welcome to your application</title>
-		<link href="https://fonts.googleapis.com/css?family=Mina" rel="stylesheet">
-		<link rel="stylesheet" href="Assets/css/Estilos.css">
-	</head>
-	<body>
-		
-		<?php require 'Parciales/header.php' ?>
-		
-		<h1>Porfavor Ingresar o Cerrar sesion</h1>
-
-		<a href="Login.php">Ingresar</a> or
-		<a href="Salir.php">Cerrar Sesion</a>
-		<a href="Registro.php">Registrate</a>
-	</body>
-	
-</html>
-</html>-->
